7ac6b40936108fd4fc38450e86451580cfb95745
abhi <abhishek.bhardwaj_cs18@gla.ac.in>
2020-02-24 16:00:32 +0530

    final commit

---
+++ PhoneBook/src/ContactsList.java
@@ -0,0 +1,195 @@
+/*
+ *   @Author: Abhishek Bhardwaj
+ */
+import java.io.Serializable;
+@SuppressWarnings("serial")
+
+public class ContactsList implements Serializable {
+    private Person head;
+    private int contactLintLength;
+
+    /*
+     *   initialize of instance for contactsList
+     */
+    public ContactsList() {
+        head = null;
+        contactLintLength = 0;
+    }
+
+    // method to add firstName,lastName,contactNumber,anotherContactNumber,emailAddress and anotherEmailAddress in String form
+    public void addInContactsLast(String firstName, String lastName, String contactNumber, String anotherContactNumber, String emailAddress) {
+        Person current = head;
+        Person previous = null;
+        Person newNode = new Person();
+
+        /*
+         setting the data
+         */
+        newNode.setFirstName(firstName);
+        newNode.setLastName(lastName);
+        newNode.setContactNumber(contactNumber);
+        newNode.setAnotherContactNumber(anotherContactNumber);
+        newNode.setE_mailAddress(emailAddress);
+
+        if (isEmpty()) {
+            head = newNode;
+            contactLintLength++;
+        } else {
+
+            /*
+             compare firstName of entries for priority by first letter in firstName
+             */
+            for (int i = 0; i < contactLintLength; i++) {
+                String[] ourNames1 = current.getFirstName().split(" ");
+                String[] ourNames2 = newNode.getFirstName().split(" ");
+
+                int result = ourNames1[ourNames1.length - 1].compareToIgnoreCase(ourNames2[ourNames2.length - 1]);
+
+                /*
+                 *  if First Name entered goes before what's stored in the head,
+                 *  new entry becomes the head
+                 */
+                if (result > 0) {
+                    if (previous == null) {
+                        newNode.setNext(current);
+                        head = newNode;
+                        contactLintLength++;
+                        break;
+                    }
+                    /*
+                     *   this section cycles through the "body" of the nodes if
+                     *   it's not the tail
+                     */
+                    previous.setNext(newNode);
+                    newNode.setNext(current);
+                    contactLintLength++;
+                    break;
+                } else
+                    /*
+                     *  if the entry replaces node at the tail,
+                     *  this entry becomes the new tail
+                     */ {
+                    if (current.getNext() == null) {
+                        current.setNext(newNode);
+                        newNode.setNext(null);
+                        contactLintLength++;
+                        break;
+                    }
+                    previous = current;
+                    current = current.getNext();
+                }
+            }
+        }
+    }
+
+    /*
+     *  method to use if the linked list is empty
+     */
+    public boolean isEmpty() {
+        return (contactLintLength == 0);
+    }
+
+    /*
+     *  method to printContactsList
+     */
+    public void printContactsList() {
+        Person tempNode = head;
+        if (head == null) {
+            System.out.println("***************************************\n|    OPPss!!  The list is empty!      |\n***************************************\n");
+        } else {
+            for (int i = 0; i < contactLintLength; i++) {
+                System.out.print("-------- * -------- * -------- * --------");
+                //System.out.print("Contact No. " + (i + 1));
+                System.out.println(tempNode);
+                tempNode = tempNode.getNext();
+                System.out.println("-------- * -------- * -------- * --------");
+            }
+
+        }
+        System.out.println();
+    }
+
+    /*
+     *  method used to search through the names in the string
+     */
+    public void searchByFirstName(String firstName) {
+        Person current = head;
+        boolean empty = true;
+        if (isEmpty()) {
+            System.out.println("***************************************\n|    OPPss!!  The list is empty!      |\n***************************************\n");
+        } else {
+            for (int i = 0; i < contactLintLength; i++) {
+                if (current.getFirstName().toLowerCase().contains(firstName.toLowerCase())) {
+                    System.out.println((i + 1) + " match found!");
+                    System.out.print("-------- * -------- * -------- * --------");
+                    System.out.println(current);
+                    //current = current.getNext();
+                    System.out.println("-------- * -------- * -------- * --------");
+                    empty = false;
+                }
+                // go to the next node
+                current = current.getNext();
+            }
+            /*
+             * if condition for contact doesn't found
+             */
+            if (empty) {
+                System.out.println("NO RESULTS FOUND!");
+            }
+        }
+
+    }
+
+    /*
+     *   method to delete contact by using index concept
+     */
+    public void displayContactsName() {
+        Person current = head;
+        System.out.println("Here are all your contacts:");
+        for (int i = 0; i < contactLintLength; i++) {
+            System.out.println((i + 1) + ". " + current.getFirstName() + " " + current.getLastName());
+            current = current.getNext();
+        }
+    }
+
+    public void deleteFromContactsList(int index) {
+        Person current = head;
+        Person previous = null;
+        boolean empty = true;
+        if (isEmpty()) {
+            System.out.println("***************************************\n|    OPPss!!  The list is empty!      |\n***************************************\n");
+        } else {
+            for (int i = 0; i < contactLintLength; i++) {
+                if ((index - 1) == i) {
+                    if (previous == null) {
+                        head = head.getNext();
+                        contactLintLength--;
+                        empty = false;
+                        break;
+                    } else if (current.getNext() == null) {
+                        previous.setNext(null);
+                        contactLintLength--;
+                        empty = false;
+                        break;
+                    } else {
+                        previous.setNext(current.getNext());
+                        contactLintLength--;
+                        empty = false;
+                        break;
+                    }
+                } else {
+                    previous = current;
+                    current = current.getNext();
+                }
+            }
+            if (empty) {
+                System.out.println("Contact Not Found");
+            } else {
+                /*
+                 * Notification of Successfully deletion of Contacts
+                 */
+                System.out.println(current.getFirstName() + " " + current.getLastName() + "'s contact deleted from list!");
+            }
+        }
+    }
+}
---
+++ PhoneBook/src/Person.java
@@ -0,0 +1,71 @@
+import java.io.Serializable;
+/*
+ * @ Author : Abhishek Bhardwaj
+ */
+/*
+ * create serialization so files can be written and read in byte form
+ * @ SuppressWarnings() - to ignore specified compiler warning in annotated element
+ */
+@SuppressWarnings("serial")
+class Person implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private String contactNumber;
+    private String anotherContactNumber;
+    private String e_mailAddress;
+    private Person next;
+
+    /*
+     *create instance of Person and set next to 'null'
+     */
+    public Person() {
+        next = null;
+    }
+
+    /*
+     *getter and setter methods of firstName , lastName , contactNumber , anotherContactNumber , emailAddress and anotherE_mailAddress
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public void setAnotherContactNumber(String anotherContactNumber) {
+        this.anotherContactNumber = anotherContactNumber;
+    }
+
+    public void setE_mailAddress(String e_mailAddress) {
+        this.e_mailAddress = e_mailAddress;
+    }
+
+    public Person getNext() {
+        return next;
+    }
+
+    public void setNext(Person next) {
+        this.next = next;
+    }
+
+    /*
+     *  toString method @Override
+     */
+    public String toString() {
+        return "\nFirst Name: " + firstName + "\nLast Name: " + lastName + "\nContact Number: " + contactNumber + " , " + anotherContactNumber + "\nEmail Address: " + e_mailAddress;
+    }
+}
